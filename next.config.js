814024ba1ad7ffae3c80812ed80f1665c0a4744b
Ian Homer <ian@purplepip.com>
2020-10-29 20:05:24 +0000

    Experiments with building

--- next.config.js
+++ next.config.js
@@ -1,15 +1,2 @@
-
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push({
-        test: /\.(ico|png|jpe?g|gif|svg)$/i,
-        loader: "file-loader",
-        options: {
-        outputPath: '../public/assets/', // if you don't use ../ it will put it inside ".next" folder by default
-        publicPath: 'assets/',
-        }
-    });
-
-    return config
-  },
-}
+const withImages = require('next-images')
+module.exports = withImages()
