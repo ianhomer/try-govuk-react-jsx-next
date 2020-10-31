cadc8cff503788a5fb12106e5b068bfa51f46b8d
Ian Homer <ian@purplepip.com>
2020-10-31 10:52:19 +0000

    remove redundant things

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images')
-const optimizedImages = require('next-optimized-images');
 const withTranspileModules = require('next-transpile-modules')(
   [
     'govuk-frontend', 
@@ -11,13 +8,6 @@ const withTranspileModules = require('next-transpile-modules')(
 );
 
 module.exports = withPlugins([
-  //[optimizedImages, {
-  //  inlineImageLimit: 16
-  //}],
-  //[withCSS],
-  //[withSass, {
-  //  cssModules: true
-  //}],
   [withImages, {
     inlineImageLimit: false,
     esModule: false
