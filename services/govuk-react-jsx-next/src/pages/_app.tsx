84760524b3e0e12139a03f68f3dddb8a75a8893f
Ian Homer <ian@purplepip.com>
2020-11-20 18:40:18 +0000

    Convert to typescript

--- services/govuk-react-jsx-next/src/pages/_app.tsx
+++ services/govuk-react-jsx-next/src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "./index.scss";
+import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import withReactRouter from "../utils/with-react-router";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+  )
 }
 
 export default withReactRouter(MyApp);
