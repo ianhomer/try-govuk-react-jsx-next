1dedc3c3e8d8079ab7417862356b32a6e648a0bc
Ian Homer <ian@purplepip.com>
2020-11-21 10:16:46 +0000

    Run prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-var-requires": "off"
+    "@typescript-eslint/no-var-requires": "off",
   },
   env: {
     jest: true,
