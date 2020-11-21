27946f50c6bc6270e98c6bb12c53eb64ecd7b4af
Ian Homer <ian@purplepip.com>
2020-11-21 14:31:27 +0000

    Add comment on babel configs

--- services/govuk-react-jsx-next/babel.config.js
+++ services/govuk-react-jsx-next/babel.config.js
@@ -1,3 +1,8 @@
+//
+// Note that babel.config.js (project wide configuration) is treated differently to
+// .babelrc (file-relative configuration) - see https://babeljs.io/docs/en/config-files.
+// Transpiling of dependent packages works better with a babel.config.js
+//
 module.exports = {
   presets: ["next/babel"],
 };
