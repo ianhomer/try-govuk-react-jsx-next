ba18e303199bfbc26378ece9b12ed85db312501e
Ian Homer <ian@purplepip.com>
2020-11-22 01:21:59 +0000

    Move hook to smoke test

--- services/govuk-react-jsx-next/src/pages/index.tsx
+++ services/govuk-react-jsx-next/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Hint } from "govuk-react-jsx";
+import { Hook } from "@try-govuk-react-jsx-next/ui-components";
 
 export default function Home() {
   return (
     <>
       <Hint>This is a hint</Hint>
+      <Hook />
     </>
   );
 }
