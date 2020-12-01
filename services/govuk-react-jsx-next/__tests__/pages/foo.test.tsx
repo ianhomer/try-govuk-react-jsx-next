8c1cfc9be6173b50a0bb47948c735aded2e9b5ab
Ian Homer <ian@purplepip.com>
2020-12-01 15:17:07 +0000

    Use enzyme to mount component

--- services/govuk-react-jsx-next/__tests__/pages/foo.test.tsx
+++ services/govuk-react-jsx-next/__tests__/pages/foo.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import Foo from "../../src/pages/foo";
+import { mount } from "enzyme";
 
 it("renders OK", () => {
   const nodes = renderer.create(<Foo />).toJSON();
   expect(nodes[0].children[0]).toBe("foo");
   expect(nodes[1].children[0]).toBe("bar");
+});
+
+it("mounts OK", () => {
+  const wrapper = mount(<Foo />);
+  expect(wrapper.find("Bar").text()).toBe("bar");
 });
