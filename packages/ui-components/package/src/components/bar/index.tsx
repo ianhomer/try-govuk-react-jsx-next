231d9826331ed473c9c35298c30e8e8fa4aef758
Ian Homer <ian@purplepip.com>
2020-12-13 11:01:25 +0000

    Add foo as nested component

--- packages/ui-components/package/src/components/bar/index.tsx
+++ packages/ui-components/package/src/components/bar/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { Foo } from "../foo";
+
 const Bar = () => {
-  return <>bar</>;
+  return (
+    <>
+      <div>bar</div>
+      <Foo />
+    </>
+  );
 };
 
 export { Bar };
