af202686611eafd19fe48fad6382c0ebc1af22d0
Ian Homer <ian.homer@gmail.com>
2021-05-22 17:27:32 +0100

    Pretty

--- services/govuk-react-jsx-next/next.config.js
+++ services/govuk-react-jsx-next/next.config.js
@@ -1,12 +1,15 @@
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
-const withTranspileModules = require("next-transpile-modules")([
-  "@try-govuk-react-jsx-next/ui-components",
-  "govuk-frontend",
-  "govuk-react-jsx",
-], {
-  resolveSymlinks: false,
-});
+const withTranspileModules = require("next-transpile-modules")(
+  [
+    "@try-govuk-react-jsx-next/ui-components",
+    "govuk-frontend",
+    "govuk-react-jsx",
+  ],
+  {
+    resolveSymlinks: false,
+  }
+);
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
